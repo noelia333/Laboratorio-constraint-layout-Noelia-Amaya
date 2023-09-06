5333d1a6b7848876481cc4bc50e836000b57f75e
noelia333 <noeliamayac@hotmail.com>
2023-09-06 18:22:10 -0500

    first commit

---
+++ app/src/main/java/co/edu/unipiloto/laboratorioconstraintlayout/MainActivity.java
@@ -0,0 +1,14 @@
+package co.edu.unipiloto.laboratorioconstraintlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.situacion_riesgo_movilidad);
+    }
+}
